27acafe33951d1e395b44faa7477a74420cfbfbd
Ziyi Huang <hzy1091149656@gmail.com>
2024-12-01 17:59:42 -0500

    feat: initialize Spring Boot project

---
+++ src/main/java/io/github/ziy1/nexevent/NexEventApplication.java
@@ -0,0 +1,13 @@
+package io.github.ziy1.nexevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ziy1/nexevent/NexEventApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ziy1.nexevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
