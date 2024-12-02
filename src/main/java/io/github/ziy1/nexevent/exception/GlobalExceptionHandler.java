645527ddd4ec9824754a37665d19638148df2b9c
Ziyi Huang <hzy1091149656@gmail.com>
2024-12-01 19:14:00 -0500

    feat(error): add global exception handling

---
+++ src/main/java/io/github/ziy1/nexevent/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package io.github.ziy1.nexevent.exception;
+
+import io.github.ziy1.nexevent.dto.ResponseMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseMessage<Void>> handleException(Exception ex, HttpServletRequest request) {
+        log.error("Exception: ", ex);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ResponseMessage.error(request.getRequestURI(), ex.getMessage()));
+    }
+}
