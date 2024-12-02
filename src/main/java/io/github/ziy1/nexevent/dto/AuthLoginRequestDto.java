1ef1aca9d8a7d3242dd5881eee8e49bf118b920d
Ziyi Huang <hzy1091149656@gmail.com>
2024-12-02 18:52:24 -0500

    style: remove unused imports

--- src/main/java/io/github/ziy1/nexevent/dto/AuthLoginRequestDto.java
+++ src/main/java/io/github/ziy1/nexevent/dto/AuthLoginRequestDto.java
@@ -1,6 +1,5 @@
 package io.github.ziy1.nexevent.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
 public record AuthLoginRequestDto(
--- src/main/java/io/github/ziy1/nexevent/dto/AuthLoginResponseDto.java
+++ src/main/java/io/github/ziy1/nexevent/dto/AuthLoginResponseDto.java
@@ -1,7 +1,5 @@
 package io.github.ziy1.nexevent.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record AuthLoginResponseDto(
         String accessToken) {
 }
