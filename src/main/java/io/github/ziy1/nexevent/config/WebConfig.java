d1433ce4008bc2b0679aae9939006e8d33bc6643
Ziyi Huang <hzy1091149656@gmail.com>
2024-12-05 20:12:11 -0500

    chore: add @NonNull annotation to addCorsMappings method parameter

--- src/main/java/io/github/ziy1/nexevent/config/WebConfig.java
+++ src/main/java/io/github/ziy1/nexevent/config/WebConfig.java
@@ -3,11 +3,12 @@ package io.github.ziy1.nexevent.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
   @Override
-  public void addCorsMappings(CorsRegistry registry) {
+  public void addCorsMappings(@NonNull CorsRegistry registry) {
     registry
         .addMapping("/**")
         .allowedOrigins("http://localhost:3000")
