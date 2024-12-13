9014cf06b86e190a97bc27b63f005791b640a375
Ziyi Huang <hzy1091149656@gmail.com>
2024-12-12 20:38:43 -0500

    feat: add RedisConfig

---
+++ src/main/java/io/github/ziy1/nexevent/config/RedisConfig.java
@@ -0,0 +1,40 @@
+package io.github.ziy1.nexevent.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+  @Value("${spring.redis.host:localhost}")
+  private String redisHost;
+
+  @Value("${spring.redis.port:6379}")
+  private int redisPort;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+    return new LettuceConnectionFactory(config);
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
+
+    // Use Jackson2JsonRedisSerializer for values
+    template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+    // Use StringRedisSerializer for keys
+    template.setKeySerializer(new StringRedisSerializer());
+
+    return template;
+  }
+}
